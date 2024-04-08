3449e2977df03a5c8e52474e03e08585223cadc2
RISHII <rishikesh.palande07@gmail.com>
2024-04-08 23:16:15 +0530

    initial commit

---
+++ src/constants/index.js
@@ -0,0 +1,70 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
+
+export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+
+export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+
+export const EDUCATION = [{
+        year: "2022 - PRESENT",
+        college: "JSMP'S Jaywantrao Sawant College Of Engineering, Pune",
+        degree: "Master's in Computer Application",
+        qualification: "Post-Graduation",
+        Percentage: ['Ongoing'],
+    },
+    {
+        year: "2019 - 2022",
+        college: "Modern College of Arts, Science and Commerce, Pune",
+        degree: "Bachelor's In Computer Science",
+        qualification: "Graduation",
+        Percentage: ['66.39%'],
+    },
+    {
+        year: "2017 - 2019",
+        college: "The Lexicon International School, Wagholi Pune",
+        degree: "Central Board of Secondary Education",
+        qualification: "High School",
+        Percentage: ['62.80%'],
+    },
+    {
+        year: "2006 - 2017",
+        college: "The Bishop’s Co-Ed School, Kalyani Nagar Pune",
+        degree: "Indian Certificate of Secondary Education",
+        qualification: "Secondary School",
+        Percentage: ['82.67%'],
+    },
+];
+
+export const PROJECTS = [{
+        title: "Digital Market place",
+        image: project1,
+        description: "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
+        technologies: ["Next.js", "Tailwind", "React", "Express.js", "MongoDB"],
+    },
+    {
+        title: "Recipe App",
+        image: project2,
+        description: "Cook-Hub is your all-in-one solution for discovering, saving, and sharing your favorite recipes. ",
+        technologies: ["HTML", "CSS", "React", "Node", "PostgreSQl"],
+    },
+    {
+        title: "Portfolio Website",
+        image: project3,
+        description: "A personal portfolio website showcasing projects, skills, and contact information.",
+        technologies: ["HTML", "React", "Tailwind"],
+    },
+    {
+        title: "Course & LMS Platform",
+        image: project4,
+        description: "A platform for creating and managing lessons, courses, quizzes and other training materials.",
+        technologies: ["Next.js", "Tailwind", "Next.js", "Express", "mySQL"],
+    },
+];
+
+export const CONTACT = {
+    address: "Rahul Co-op Society Road no. 14, Tingre Nagar Pune-411032",
+    phoneNo: "+91 8459972812 ",
+    email: "rishikesh.palande07@gmail.com",
+};
